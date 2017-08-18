1f2d60720d26e76d128512dd60eaa7de6aafe3cf
Ali Can İPEK <alicanipek@outlook.com.tr>
2017-08-18 15:05:09 +0300

    Problem 3'ün çözümü eklendi.

---
+++ euler3.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func primeFactors(x int) []int {
+	//var sound string
+	y := make([]int, 0)
+	for x > 1 {
+		for i := 2; i <= x; i++ {
+			if x%i == 0 {
+				y = append(y, i)
+				x = x / i
+				break
+			}
+		}
+	}
+	return y
+}
+
+func max(x []int) int {
+	maximum := x[0]
+	for _, v := range x[1:] {
+		if v > maximum {
+			maximum = v
+		}
+	}
+	return maximum
+}
+
+func main() {
+	fmt.Printf("Prime factors of 600851475143 are : %v \n", primeFactors(600851475143))
+	fmt.Printf("Largest prime factor of the number 600851475143 is : %d", max(primeFactors(600851475143)))
+}
